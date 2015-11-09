06f1dc050fead84a68bd0bd36d018bebd9bef9fa
rohrerb <brandon.rohrer@outlook.com>
2015-11-09 09:18:00 -0500

    Finalized Zip task and made small changes to other tasks.

---
+++ VSO Tasks/Zip/README.md
@@ -0,0 +1,24 @@
+### How to use **Zip** build / release tasks
+
+Follow the below steps to upload this task to your account:
+
+* Download the tasks repo as [zip file] or clone it using git:
+* Extract the zip file of the desired task
+* Open command prompt and navigate to the folder that contains a json and powershell files.
+
+## Install / Upload Custom Task
+
+*Follow instructions on how to upload the task using - https://github.com/TotalALM/VSO-Tasks
+
+## How to use
+
+* Add a new task and select Zip.
+
+![tfs-cli](docs/SelectTask.png "Build Task")
+
+* Input or Select Folder to compress.
+* Input Zip file to compres the folder into. ()This can be new or existing.)
+* Remove Folder After Compression - When checked, the folder will be removed after the compression is complete.
+
+![tfs-cli](docs/Values.png "Options")
+
---
+++ VSO Tasks/Zip/task/zip.ps1
@@ -0,0 +1,42 @@
+﻿[CmdletBinding(DefaultParameterSetName = 'None')]
+param
+(
+    [String] [Parameter(Mandatory = $true)]
+    $folder,
+	[String] [Parameter(Mandatory = $true)]
+    $zip,
+    [String] [Parameter(Mandatory = $false)]
+    $removeFolderAfterExtraction
+)
+
+Write-Verbose "Entering script Extract.ps1"  -Verbose
+
+Write-Verbose "folder = $folder" -Verbose 
+Write-Verbose "zip = $zip" -Verbose 
+Write-Verbose "removeFolderAfterExtraction = $removeFolderAfterExtraction" -Verbose 
+
+function CreateZip($folderPath, $zipPath)
+{ 
+    Add-Type -Assembly "System.IO.Compression.FileSystem" ;
+	[System.IO.Compression.ZipFile]::CreateFromDirectory("$folderPath", "$zipPath") ;
+    
+}
+
+function RemoveFolder($folder)
+{ 
+Write-Verbose "Removing = $folder" -Verbose 
+
+start-Sleep -m 4000
+If (Test-Path $folder){
+	    Remove-Item $folder -Recurse -Force
+    }
+}
+
+#Create the zip file. 
+CreateZip -zipPath $zip -folderPath $folder
+
+#Remove the folder
+If ([System.Convert]::ToBoolean($removeFolderAfterExtraction))
+{
+    RemoveFolder($folder)  
+}
