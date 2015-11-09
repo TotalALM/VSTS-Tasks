ced16be95ab3b03a57356863de4b0178407b5e11
rohrerb <brandon.rohrer@outlook.com>
2015-11-09 09:05:39 -0500

    Updated Tasks and Readme's

---
+++ VSO Tasks/Unzip/README.md
@@ -0,0 +1,24 @@
+### How to use **UnZip** build / release tasks
+
+Follow the below steps to upload this task to your account:
+
+* Download the tasks repo as [zip file] or clone it using git:
+* Extract the zip file of the desired task
+* Open command prompt and navigate to the folder that contains a json and powershell files.
+
+## Install / Upload Custom Task
+
+*Follow instructions on how to upload the task using - https://github.com/TotalALM/VSO-Tasks
+
+## How to use
+
+* Add a new task and select UnZip.
+
+![tfs-cli](docs/SelectTask.png "Build Task")
+
+* Input or Select Zip file to extract.
+* Input Target Folder to extract the zip file into.
+* Remove Zip After Extraction - When checked, the zip file will be removed after the extraction is complete.
+
+![tfs-cli](docs/TokenizationValues.png "Options")
+
---
+++ VSO Tasks/Unzip/task/unzip.ps1
@@ -0,0 +1,49 @@
+﻿[CmdletBinding(DefaultParameterSetName = 'None')]
+param
+(
+    [String] [Parameter(Mandatory = $true)]
+    $zip,
+    [String] [Parameter(Mandatory = $true)]
+    $folder,
+    [String] [Parameter(Mandatory = $false )]
+    $removeZipAfterCompress
+)
+
+Write-Verbose "Entering script UnZip.ps1"  -Verbose
+
+Write-Verbose "folder = $folder" -Verbose 
+Write-Verbose "zip = $zip" -Verbose 
+Write-Verbose "removeZipAfterCompress = $removeZipAfterCompress" -Verbose 
+
+function UnZip($zipPath, $folderPath)
+{
+    Add-Type -Assembly "System.IO.Compression.FileSystem" ;
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$zipPath", "$folderPath") ;
+    
+    Start-Sleep -m 4000
+    
+    If (Test-Path $zipPath){
+	    Remove-Item $zipPath
+    }
+
+}
+
+function RemoveZip($zip)
+{ 
+
+Write-Verbose "Removing = $zip" -Verbose 
+
+start-Sleep -m 4000
+If (Test-Path $zip){
+	    Remove-Item $zip -Recurse -Force
+    }
+}
+
+#Extract the zip file. 
+Unzip -zipPath $zip -folderPath $folder
+
+#Remove the folder
+If ([System.Convert]::ToBoolean($removeZipAfterCompress))
+{
+    RemoveZip($zip)  
+}
